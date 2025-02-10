96e1c5cb629eaad876fc51214df378dda1196f3d
meng lei <1955854667@qq.com>
2025-02-10 22:15:24 +0800

    feat: 删除不必要的文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
     vue(),
-    ElementPlus({
-      format: 'esm',
+    AutoImport({
+      resolvers:[ElementPlusResolver()]
+    }),
+    Components({
+      resolvers:[ElementPlusResolver()]
     })
   ],
   server: {
