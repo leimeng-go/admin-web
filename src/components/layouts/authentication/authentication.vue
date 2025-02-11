1f4f35c79ef35ed30bce26b105428e1926853381
meng lei <1955854667@qq.com>
2025-02-12 00:03:27 +0800

    feat: add toolbar

--- src/components/layouts/authentication/authentication.vue
+++ src/components/layouts/authentication/authentication.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { ToolbalType } from './types';
+import { ref } from 'vue';
+
 
 interface Props {
     appName?: string;
@@ -8,6 +11,39 @@ interface Props {
     sloganImage?: string;
     toolbar?: boolean;
     copyright?: boolean;
-    toolbarList?: 
+    toolbarList?: ToolbalType[];
 }
+
+withDefaults(defineProps<Props>(),{
+    appName: '',
+    copyright: true,
+    log:'',
+    pageDescription: '',
+    pageTitle: '',
+    sloganImage: '',
+    toolbar: true,
+    toolbarList: ()=>['color','language','layout','theme'],
+});
+
+// TODO: 首页布局
+const authPanelCenter = ref('')
+const authPanelLeft = ref('')
+const authPanelRight = ref('')
+const isDark = ref('auto')
+
+
 </script>
+
+<template>
+    <div
+      :class="[isDark]"
+      class="flex min-h-full flex-1 select-none overflow-hidden"
+    >
+      <template v-if="toolbar">
+         <slot name="toolbar">
+
+         </slot>
+      </template>
+
+    </div>
+</template>
---
+++ src/components/layouts/authentication/toolbar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { ToolbalType } from './types';
+import { computed } from 'vue';
+
+interface Props {
+    toolbarList?: ToolbalType[];
+}
+
+defineOptions({
+    name: 'AuthenticationToolbal',
+})
+
+const props = withDefaults(defineProps<Props>(),{
+    toolbarList: () => ['color','language','layout', 'theme']
+})
+
+const showColor = computed(() => props.toolbarList.includes('color'))
+const showLayout = computed(() => props.toolbarList.includes('layout'))
+const showLanguage = computed(() => props.toolbarList.includes('language'))
+const showTheme = computed(() => props.toolbarList.includes('theme'))
+
+</script>
+
+<template>
+    <div
+      :class="{
+        'bg-accent rounded-3xl px-3 py-1': toolbarList.length >1,
+      }"
+    >
+
+    </div>
+</template>
